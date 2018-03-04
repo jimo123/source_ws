5679538af949bcc70de4e19d1caaf4f39aa04ce6
iim <iim>
2018-03-04 19:22:11 +0800

    can build map only use lidar

--- cartographer_ros/cartographer_ros/configuration_files/iim_map.lua
+++ cartographer_ros/cartographer_ros/configuration_files/iim_map.lua
@@ -37,7 +37,7 @@ options = {
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
-TRAJECTORY_BUILDER_2D.use_imu_data = true
+TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 SPARSE_POSE_GRAPH.optimization_problem.huber_scale = 1e2
 
